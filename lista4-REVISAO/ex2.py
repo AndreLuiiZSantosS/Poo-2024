ebb649e814138d6bc54a9824123b27a52c923a97
AndreLuiiZSantosS <104994626+AndreLuiiZSantosS@users.noreply.github.com>
2024-10-31 07:47:07 -0300

    lista4 exercicio2

---
+++ lista4-REVISAO/ex2.py
@@ -0,0 +1,8 @@
+def maior (x, y, z):
+  return max (x, y, z)
+
+num1 float(input("Digite o primeiro numero: "))
+num2 float(input("Digite o segundo numero: "))
+num3 float(input("Digite o terceiro numero: "))
+
+print ("O maior numero é: " , maior(num1, num2, num3))
